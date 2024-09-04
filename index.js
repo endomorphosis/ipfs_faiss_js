5c49390951db5aa910c2d72fa094421330e570a5
endomorphosis <starworks5@gmail.com>
2024-09-04 16:44:53 -0700

    Refactor export statements in test.js and add test_ipfs_faiss_js export

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ export { ipfsFaissJs } from './ipfs_faiss_js/main.js'
 export { ipfsFaissJs as default } from './ipfs_faiss_js/main.js'
 export * from './test/test.js'
 export { test_ipfs_faiss_js } from './test/test.js'
+import { test_ipfs_faiss_js } from './test/test.js'
 const test = new test_ipfs_faiss_js();
 export { test }
